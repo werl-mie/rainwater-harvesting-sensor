6e6967bf07258d2f73c06f75ecce5a5305f4095b
Eren Rudy <eren@rudy-web.ca>
2025-10-04 13:40:41 -0400

    Added nrf_cloud sample

---
+++ v3/nrf-thingy-91x/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/platform/target/cmake_install.cmake
@@ -0,0 +1,109 @@
+# Install script for directory: /opt/nordic/ncs/v3.0.0/nrf/modules/trusted-firmware-m/tfm_boards/nrf9120
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "MinSizeRel")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/opt/nordic/ncs/toolchains/b8efef2ad5/opt/zephyr-sdk/arm-zephyr-eabi/bin/arm-zephyr-eabi-objdump")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/platform/target/nrf91/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/platform/target/tfm_board/cmake_install.cmake")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/cpuarch.cmake")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform" TYPE FILE MESSAGE_NEVER RENAME "cpuarch.cmake" FILES "/opt/nordic/ncs/v3.0.0/nrf/modules/trusted-firmware-m/tfm_boards/nrf9120/ns/cpuarch_ns.cmake")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/common/nrf9120/cpuarch.cmake")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/common/nrf9120" TYPE FILE MESSAGE_NEVER FILES "/opt/nordic/ncs/v3.0.0/modules/tee/tf-m/trusted-firmware-m/platform/ext/target/nordic_nrf/common/nrf9120/cpuarch.cmake")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/config.cmake")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform" TYPE FILE MESSAGE_NEVER FILES "/opt/nordic/ncs/v3.0.0/nrf/modules/trusted-firmware-m/tfm_boards/nrf9120/config.cmake")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/../common/config.cmake")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/../common" TYPE FILE MESSAGE_NEVER FILES "/opt/nordic/ncs/v3.0.0/nrf/modules/trusted-firmware-m/tfm_boards/nrf9120/../common/config.cmake")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/tests")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/Users/erudy/werl/post-masc/thingy91/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform" TYPE DIRECTORY MESSAGE_NEVER FILES "/opt/nordic/ncs/v3.0.0/modules/tee/tf-m/trusted-firmware-m/platform/ext/target/nordic_nrf/nrf9161dk_nrf9161/tests")
+endif()
+
