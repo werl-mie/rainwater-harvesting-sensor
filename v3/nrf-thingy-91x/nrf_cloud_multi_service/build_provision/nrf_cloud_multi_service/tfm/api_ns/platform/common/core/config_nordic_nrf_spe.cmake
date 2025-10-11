7701686beade8829240477db7b04239c23a037ed
Eren Rudy <eren@rudy-web.ca>
2025-10-11 13:35:15 -0400

    Remove build files in gitignore

--- v3/nrf-thingy-91x/nrf_cloud_multi_service/build_provision/nrf_cloud_multi_service/tfm/api_ns/platform/common/core/config_nordic_nrf_spe.cmake
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (c) 2023, Nordic Semiconductor ASA.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-
-set(HAL_NORDIC_PATH /opt/nordic/ncs/v3.0.0/modules/hal/nordic CACHE STRING "Path to nordic HAL" FORCE)
