09124294a930a1835fb2c7df9fba298614d747f3
clairezhang2003 <clairezhang2023@gmail.com>
2026-02-20 16:30:11 -0500

    SENDING DATA TO RX
    CAN'T GET THE PRINTING TO WORK ON THIS SERIAL MONITOR, BUT ALL STATUS ON THE RX SERIAL MONITOR

--- v4_CZ/TX/LORA_TX.ino
+++ v4_CZ/TX/LORA_TX.ino
@@ -1,33 +1,102 @@
-#include <Arduino.h>
+#include "ArduinoLowPower.h"
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_SleepyDog.h>
 #include "GroveLora.h"
-#include <RTCZero.h>
+
+#define PIN_SNS_BUCKET 12      
+#define PIN_SNS_LEAFSWITCH 6  
+#define PIN_SNS_FLOATTOP 11    
+#define PIN_SNS_FLOATBOT 9    
 
 GroveLora dev;
-RTCZero rtc;
-SensorDatum_t myData = {0, 4, 3, 6, 0};
+volatile bool flag_event_change = false;
+volatile int bucketCount = 0;
+int lastLeaf, lastTop, lastBot;
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // --- 10S SAFETY FLASHING WINDOW ---
+  // LED blinks fast: This is the time to upload new code!
+  for (int i = 0; i < 40; i++) {
+    digitalWrite(LED_BUILTIN, HIGH); delay(125);
+    digitalWrite(LED_BUILTIN, LOW); delay(125);
+  }
+
   Serial.begin(115200);
-  Serial1.begin(9600);
-  while(!Serial && millis() < 5000); 
-  
-  rtc.begin();
-  Serial.println("TX STARTING...");
+  Serial1.begin(9600); 
   
   dev.sendCmd("AT+MODE=TEST\r\n"); delay(200);
   dev.sendCmd("AT+TEST=RFCFG,915,SF7,125,12,15,14,ON,OFF,OFF\r\n"); delay(200);
-  Serial.println("TX CONFIGURED (915MHz)");
+
+  pinMode(PIN_SNS_BUCKET, INPUT_PULLUP);
+  pinMode(PIN_SNS_LEAFSWITCH, INPUT_PULLUP);
+  pinMode(PIN_SNS_FLOATTOP, INPUT_PULLUP);
+  pinMode(PIN_SNS_FLOATBOT, INPUT_PULLUP);
+
+  lastLeaf = digitalRead(PIN_SNS_LEAFSWITCH);
+  lastTop  = digitalRead(PIN_SNS_FLOATTOP);
+  lastBot  = digitalRead(PIN_SNS_FLOATBOT);
+
+  LowPower.attachInterruptWakeup(PIN_SNS_BUCKET, isr_bucket, FALLING);
+  LowPower.attachInterruptWakeup(PIN_SNS_LEAFSWITCH, isr_generic, CHANGE);
+  LowPower.attachInterruptWakeup(PIN_SNS_FLOATTOP, isr_generic, CHANGE);
+  LowPower.attachInterruptWakeup(PIN_SNS_FLOATBOT, isr_generic, CHANGE);
+  
+  Serial.println("--- TX READY (EVENT-ONLY MODE) ---");
 }
 
 void loop() {
-  myData.ts = rtc.getEpoch();
-  myData.data = analogRead(A0);
+  if (flag_event_change) {
+    // Reactivate USB Serial
+    Serial.begin(115200);
+    unsigned long startWait = millis();
+    while(!Serial && (millis() - startWait < 500)); 
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    processSensors(); 
+    
+    Serial.println(F(">>> DATA SENT. SLEEPING..."));
+    Serial.flush();
+    
+    delay(2000); // Window to see the print
+    digitalWrite(LED_BUILTIN, LOW);
+    flag_event_change = false;
+  }
   
+  Watchdog.reset();
+  LowPower.sleep(); 
+}
+
+void isr_bucket() {
+  static unsigned long last_tip = 0;
+  if (millis() - last_tip > 200) { bucketCount++; flag_event_change = true; }
+  last_tip = millis();
+}
+
+void isr_generic() { flag_event_change = true; }
+
+void processSensors() {
+  static int lastSentBucket = 0;
+  if (bucketCount > lastSentBucket) { transmit(1, bucketCount); lastSentBucket = bucketCount; }
+
+  int cLeaf = digitalRead(PIN_SNS_LEAFSWITCH);
+  if (cLeaf != lastLeaf) { transmit(2, cLeaf); lastLeaf = cLeaf; }
+
+  int cTop = digitalRead(PIN_SNS_FLOATTOP);
+  if (cTop != lastTop) { transmit(3, cTop); lastTop = cTop; }
+
+  int cBot = digitalRead(PIN_SNS_FLOATBOT);
+  if (cBot != lastBot) { transmit(4, cBot); lastBot = cBot; }
+}
+
+void transmit(uint8_t typeID, uint16_t value) {
+  Serial.print(F("EVENT: Type=")); Serial.print(typeID);
+  Serial.print(F(" Val=")); Serial.println(value);
+
   char cmd[128];
-  sprintf(cmd, "AT+TEST=TXLRPKT,\"%08x%04x%02x%02x%04x\"\r\n", 
-          myData.ts, myData.site_id, myData.node_id, myData.sensor_type, myData.data);
-  
-  Serial.print("Sending A0: "); Serial.println(myData.data);
+  sprintf(cmd, "AT+TEST=TXLRPKT,\"00000000000403%02X%04X\"\r\n", typeID, value);
   dev.sendCmd(cmd);
-  delay(5000); 
+  delay(1000); 
 }
